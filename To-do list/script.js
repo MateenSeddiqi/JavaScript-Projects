1fcb49245fa652c4ab065db4e65002876b6fef21
Seddiqui <ab.mateenseddiqui@gmail.com>
2024-06-08 18:24:46 +0430

    tasks checked and remove task feature added

--- To-do list/script.js
+++ To-do list/script.js
@@ -16,3 +16,13 @@ function addTask(){
     }
     userInput.value = '';
 }
+
+
+displayBox.addEventListener('click', function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle('checked');
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
