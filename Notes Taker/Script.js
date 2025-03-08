ada836057df517ca8c3550e1b3a1f77759ef1214
Seddiqui <ab.mateenseddiqui@gmail.com>
2025-03-08 23:04:09 +0430

    refine the JS code

--- Notes Taker/Script.js
+++ Notes Taker/Script.js
@@ -1,19 +1,19 @@
 const notesContainer = document.querySelector(".notes-container");
-        const createBtn = document.querySelector(".create-note-btn");
+const createBtn = document.querySelector(".create-note-btn");
 
-        createBtn.addEventListener("click", () => {
-            const note = document.createElement("div");
-            note.classList.add("note");
-            note.innerHTML = `
-                <textarea class="input-box" placeholder="Write your note..."></textarea>
-                <div class="note-action">
-                    <button class="delete-button">Delete</button>
-                </div>
-            `;
-            notesContainer.appendChild(note);
+createBtn.addEventListener("click", () => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+        <textarea class="input-box" placeholder="Write your note..."></textarea>
+        <div class="note-action">
+            <button class="delete-button">Delete</button>
+        </div>
+        `;
+        notesContainer.appendChild(note);
 
-            // Add delete functionality
-            note.querySelector(".delete-button").addEventListener("click", () => {
-                note.remove();
-            });
+        // Add delete functionality
+    note.querySelector(".delete-button").addEventListener("click", () => {
+        note.remove();
         });
+});
