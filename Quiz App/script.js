bbdacc96488a7d29908e4d49beb7ed31ba6e5965
Seddiqui <ab.mateenseddiqui@gmail.com>
2026-01-20 13:36:14 +0430

    working on questions

---
+++ Quiz App/script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Which is the biggest animal in the world?",
+        answers:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers:[
+            {text: "Vatican city", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Afghanistan", correct: false},
+            {text: "USA", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers:[
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+
+    }
+]
