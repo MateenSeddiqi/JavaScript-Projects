e4a06451e84b8956ffd76389687b7a5e976470cc
Seddiqui <ab.mateenseddiqui@gmail.com>
2026-01-20 16:01:57 +0430

    working on js

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -27,13 +27,42 @@ const questions = [
         ]
     },
     {
-        question: "Which is the smallest country in the world?",
+        question: "Which is the smallest continent in the world?",
         answers:[
-            {text: "Shark", correct: false},
-            {text: "Blue Whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
         ]
 
     }
 ]
+
+
+const questionElement= document.getElementById("question");
+const answerElement = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex= 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answers => {
+        const button = document.createElement("button");
+        button.innerHTML = answers.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
