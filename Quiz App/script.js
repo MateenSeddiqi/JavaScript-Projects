c48b6b99697e57f4ea68fe00cdf5b60b2eeadb5c
Seddiqui <ab.mateenseddiqui@gmail.com>
2026-01-21 13:52:42 +0430

    JS tasks is done

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -73,27 +73,55 @@ function showQuestion(){
 }
 
 function resetState(){
-    nextButton.style.display = "None";
+    nextButton.style.display = "none";
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
 
 function selectAnswer(e){
-    const selectBtn = e.target;
-    const isCorrect = selectBtn.dataset.correct === "true";
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
     if (isCorrect){
-        selectBtn.classList.add("correct");
-    }else{
-        selectBtn.classList.add("incorrect");
+        selectedBtn.classList.add("correct");
+        score++; 
+    } else {
+        selectedBtn.classList.add("incorrect");
     }
+
     Array.from(answerButtons.children).forEach(button => {
         if(button.dataset.correct === "true"){
             button.classList.add("correct");
         }
         button.disabled = true;
     });
+
     nextButton.style.display = "block";
 }
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = 'Play Again';
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
 
 startQuiz();
